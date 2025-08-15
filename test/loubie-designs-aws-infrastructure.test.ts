d56bce5cd919aaafabad76cffcad522dd92b4304
rhettbarton <rhett.bart@gmail.com>
2025-08-14 20:34:26 -0600

    cdk init app --language=typescript

---
+++ test/loubie-designs-aws-infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LoubieDesignsAwsInfrastructure from '../lib/loubie-designs-aws-infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/loubie-designs-aws-infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LoubieDesignsAwsInfrastructure.LoubieDesignsAwsInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
