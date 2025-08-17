740a43472e3911d6df72566aa9c747af6e1aa93c
rhettbarton <rhett.bart@gmail.com>
2025-08-17 14:10:26 -0600

    add unit tests

--- test/loubie-designs-aws-infrastructure.test.ts
+++ test/loubie-designs-aws-infrastructure.test.ts
@@ -1,17 +1,83 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as LoubieDesignsAwsInfrastructure from '../lib/loubie-designs-aws-infrastructure-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/loubie-designs-aws-infrastructure-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new LoubieDesignsAwsInfrastructure.LoubieDesignsAwsInfrastructureStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { LoubieDesignsInfrastructureStack } from '../lib/loubie-designs-aws-infrastructure-stack';
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+describe('Loubie Designs Infrastructure Stack', () => {
+	let app: cdk.App;
+	let stack: LoubieDesignsInfrastructureStack;
+	let template: Template;
+
+	beforeAll(() => {
+		app = new cdk.App({
+			context: { environment: 'dev' },
+		});
+		stack = new LoubieDesignsInfrastructureStack(app, 'TestStack');
+		template = Template.fromStack(stack);
+	});
+
+		test('S3 bucket is created with correct properties', () => {
+			template.hasResourceProperties('AWS::S3::Bucket', {
+				CorsConfiguration: {
+					CorsRules: [
+						{
+							AllowedOrigins: [
+								'http://localhost:5173',
+								'http://localhost:3000',
+							],
+							AllowedMethods: ['GET'],
+							AllowedHeaders: ['*'],
+							MaxAge: 3600,
+						},
+					],
+				},
+				PublicAccessBlockConfiguration: {
+					BlockPublicAcls: true,
+					BlockPublicPolicy: true,
+					IgnorePublicAcls: true,
+					RestrictPublicBuckets: true,
+				},
+			});
+		});
+
+		test('DynamoDB table is created with correct properties', () => {
+			template.hasResourceProperties('AWS::DynamoDB::Table', {
+				BillingMode: 'PAY_PER_REQUEST',
+				PointInTimeRecoverySpecification: {
+					PointInTimeRecoveryEnabled: true,
+				},
+				KeySchema: [
+					{ AttributeName: 'id', KeyType: 'HASH' },
+				],
+			});
+		});
+
+		test('CloudFront distribution is created', () => {
+			template.resourceCountIs('AWS::CloudFront::Distribution', 1);
+			template.hasResourceProperties('AWS::CloudFront::Distribution', {
+				DistributionConfig: {
+					Enabled: true,
+					PriceClass: 'PriceClass_100',
+					DefaultCacheBehavior: {
+						ViewerProtocolPolicy: 'redirect-to-https',
+					},
+				},
+			});
+		});
+
+		test('IAM role for Amplify is created with correct policies', () => {
+			template.hasResourceProperties('AWS::IAM::Role', {
+				AssumeRolePolicyDocument: {
+					Statement: [
+						{
+							Effect: 'Allow',
+							Action: 'sts:AssumeRole',
+							Principal: { Service: 'amplify.amazonaws.com' },
+						},
+					],
+					Version: '2012-10-17',
+				},
+				Description: 'Execution role for Loubie Designs Amplify app',
+			});
+		});
 });
